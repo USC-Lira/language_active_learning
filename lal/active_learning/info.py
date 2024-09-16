b18e0aaabcac706bddded12dfd0a17e93eb7baf3
eisukehirota <eh_352@usc.edu>
2024-09-16 23:22:33 +0000

    feat: updated info gain so cannot use a previously utilized query

--- lal/active_learning/info.py
+++ lal/active_learning/info.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-def info(w_samples, l_samples, traj_embeds):
+def info(w_samples, l_samples, traj_embeds, prev_idxs):
 	'''
 	Batched matrix algebra for information gain equation
 	parameters:
@@ -12,26 +12,25 @@ def info(w_samples, l_samples, traj_embeds):
 		idx (type int): the index of the next trajectory to query the human that provides the best info gain
 		ig (type float): the max amount of info gained
 	'''
+	traj_embeds = np.delete(traj_embeds, prev_idxs, 0)
 	M = w_samples.shape[0] # w/ shape (M, dim)
 	dim = w_samples.shape[1]
 	K = l_samples.shape[1] # w/ shape (M, K, dim)
 	T = traj_embeds.shape[0] # (T, dim)
 
-	# embed_diff = (w_samples.unsqueeze(1) - traj_embeds.unsqueeze(0)).reshape(M, T, dim) # shape (M, T, dim)
-	# align = torch.einsum('mtd,mqd->mtq', embed_diff, l_samples) # shape (M, T, K) # it shouldn't be (M, T, K) bc this only allows l's to multiply w/ the w it was sampled from, but not w/ other w's
-	# numerator = torch.exp(torch.transpose(align, 0, 1)) # shape (T, M, K)
-
 	embed_diff = (w_samples.unsqueeze(1) - traj_embeds.unsqueeze(0)).reshape(M*T, dim) # shape (M*T, dim)
 	align = np.exp(embed_diff @ l_samples.reshape(M*K, dim).T) # shape (M*T, M*K)
-	probs = np.maximum(align.reshape(M, T, -1).transpose(0, 1), 1e-8) # shape (T, M, M*K), so: (trajectory, weight, language)
-	# probs += torch.abs(torch.min(probs, dim=-1)[0].reshape(T, M, 1)) # normalize values to make everything positive
-	probs /= torch.sum(probs, dim=-1).unsqueeze(-1) # shape (T, M, M*K)
-	# tmp = torch.nn.functional.relu(M * probs / torch.sum(probs, dim=1).unsqueeze(1)) + eps
+	probs = align.reshape(M, T, -1).transpose(0, 1) # shape (T, M, M*K), so: (trajectory, weight, language)
+	probs /= torch.sum(probs, dim=-1).unsqueeze(-1) # shape (T, M, M*K) # this step is here b/c the prob fxn is actually equal to the numerator divide by all possible language
 	tmp = M * probs / torch.sum(probs, dim=1).unsqueeze(1)
 	f_values = torch.sum(torch.sum(torch.log2(tmp), dim=2), dim=1) / (M*K) # shape (T)
 	
 	idx = torch.argmax(f_values) # easy code uses argmin but also divides by negative M for some reason
 	ig = torch.abs(torch.nan_to_num(f_values[idx], nan=1))
+
+	for i in prev_idxs:
+		if idx >= i: idx += 1
+		else: break
 	return idx, ig
 
 
